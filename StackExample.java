eba70a14edb4ad63d03e637d0a9dc13db660222b
Mahendra <mahendrakumar27697@gmail.com>
2025-03-18 01:25:23 +0530

    Collection exapmles

---
+++ StackExample.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(6);
+        stack.push(2);
+
+        System.out.println(stack);
+
+        Integer RemovedElement = stack.pop();
+
+        System.out.println(stack);
+
+        stack.add(3);
+        Integer peekElement  = stack.peek();
+
+        //also can use vactor inteface methods
+        stack.add(3, 4);
+    }
+}
